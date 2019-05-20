82a735fa61758b8667deb1e73e3d91209fcb4e96
Karam EL HABOUTI <karam.elhabouti@hiptest.net>
2019-05-20 10:07:16 +0200

    Target every browser with at least 1% global usage
    Includes IE11

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,12 +1,20 @@
 module.exports = {
   use: [
     '@neutrinojs/standardjs',
-    '@neutrinojs/react-components',
+    [
+      '@neutrinojs/react-components',
+      {
+        targets: {
+          browsers: ['> 1%']
+        }
+      }
+    ],
     [
       '@neutrinojs/jest',
       {
-        testRegex: 'src/.*(_test|_spec|\\.test|\\.spec)\\.(js|jsx|vue|ts|tsx|mjs)$',
-        setupTestFrameworkScriptFile: '<rootDir>/test-setup.js',
+        testRegex:
+          'src/.*(_test|_spec|\\.test|\\.spec)\\.(js|jsx|vue|ts|tsx|mjs)$',
+        setupTestFrameworkScriptFile: '<rootDir>/test-setup.js'
       }
     ]
   ]
