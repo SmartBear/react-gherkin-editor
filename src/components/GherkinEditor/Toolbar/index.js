ba64b462fb133fcb35799236ebfbb1d20914fe76
Timothée Ville <timothee.ville@hiptest.net>
2020-12-08 17:22:36 +0100

    Transform react toolbar class into functional component

--- src/components/GherkinEditor/Toolbar/index.js
+++ src/components/GherkinEditor/Toolbar/index.js
@@ -1,62 +1,53 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import Select from '@atlaskit/select'
 import gherkinLanguages from '../modules/gherkin_languages'
 import { ToolbarContainer, LanguageDropdownContainer } from './style'
-const availableLanguages = Object.keys(gherkinLanguages).map(key => ({
-  label: gherkinLanguages[key].native,
-  value: key
-}))
 
-const languageSelectStyles = {
-  container (styles) {
-    return { ...styles, 'z-index': 5 }
-  }
-}
+const Toolbar = ({ content, defaultLanguage, readOnly, onLanguageChange, setModeLanguage }) => {
+  const [language, setLanguage] = useState(defaultLanguage)
+  const { key, native } = gherkinLanguages[language]
 
-class Toolbar extends PureComponent {
-  state = {
-    language: this.props.defaultLanguage
-  }
+  useEffect(() => {
+    if (defaultLanguage !== language) {
+      setLanguage(defaultLanguage)
+    }
+  }, [defaultLanguage])
 
-  languageChangeHandler = (option, _event) => {
-    const { onLanguageChange } = this.props
+  useEffect(() => {
+    setModeLanguage(language)
+  }, [language])
+
+  const languageChangeHandler = (option, _event) => {
     const { value } = option
-    this.setState({ language: value })
+    setLanguage(value)
     onLanguageChange(option)
   }
 
-  /* eslint-disable-next-line camelcase */
-  UNSAFE_componentWillReceiveProps (nextProps) {
-    if (nextProps.defaultLanguage !== this.state.language) {
-      this.setState({ language: nextProps.defaultLanguage })
+  const availableLanguages = Object.keys(gherkinLanguages).map(key => ({
+    label: gherkinLanguages[key].native,
+    value: key
+  }))
+
+  const languageSelectStyles = {
+    container (styles) {
+      return { ...styles, 'z-index': 5 }
     }
   }
 
-  componentDidUpdate (_prevProps, prevState) {
-    if (prevState.language !== this.state.language) {
-      this.props.setModeLanguage(this.state.language)
-    }
-  }
-
-  render () {
-    const { content, readOnly } = this.props
-    const { language } = this.state
-    const { key, native } = gherkinLanguages[language]
-    return (
-      <ToolbarContainer>
-        <LanguageDropdownContainer>
-          <Select
-            value={{ key: key, label: native }}
-            options={availableLanguages}
-            onChange={this.languageChangeHandler}
-            styles={languageSelectStyles}
-            isDisabled={readOnly}
-          />
-        </LanguageDropdownContainer>
-        {content}
-      </ToolbarContainer>
-    )
-  }
+  return (
+    <ToolbarContainer>
+      <LanguageDropdownContainer>
+        <Select
+          value={{ key: key, label: native }}
+          options={availableLanguages}
+          onChange={languageChangeHandler}
+          styles={languageSelectStyles}
+          isDisabled={readOnly}
+        />
+      </LanguageDropdownContainer>
+      {content}
+    </ToolbarContainer>
+  )
 }
 
 export default Toolbar
