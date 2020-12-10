d0e56401dd8e868f4814e6535394156b721d1aa5
Timothée Ville <timothee.ville@hiptest.net>
2020-12-10 14:38:28 +0100

    Upgrade neutrino from v8 to v9

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+
+const neutrino = require('neutrino')
+
+// Set a default NODE_ENV before loading any middleware
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
+
+module.exports = neutrino().jest()
