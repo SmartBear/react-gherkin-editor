8e00a977826b0ae0b3256e85d0479c1d44357388
Karam EL HABOUTI <karamosky@gmail.com>
2021-01-18 12:20:40 +0100

    Move more files to TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import GherkinEditor from 'components/GherkinEditor'
 import GherkinLinter from 'lib/gherkin-linter'
 
-export {
-  GherkinLinter
-}
+export { GherkinLinter }
 
 export default GherkinEditor
