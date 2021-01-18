14e500ac69b9d0e5af7615a52b83e8e2da4a5238
Karam EL HABOUTI <karamosky@gmail.com>
2021-01-18 18:44:13 +0100

    Remove duplicate imports

--- src/lib/gherkin-languages/index.ts
+++ src/lib/gherkin-languages/index.ts
@@ -1,4 +1,5 @@
-import languages from '@cucumber/gherkin/dist/src/gherkin-languages.json'
+import Languages from '@cucumber/gherkin/dist/src/gherkin-languages.json'
 
-export { default } from '@cucumber/gherkin/dist/src/gherkin-languages.json'
-export type LanguageIdentifier = keyof typeof languages
+export type LanguageIdentifier = keyof typeof Languages
+
+export default Languages
