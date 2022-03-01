b898864d1d7e2efe51ed98746d27243c6971cd33
Timothée Ville <timothee.ville@smartbear.com>
2022-03-01 10:28:30 +0100

    Import debounce from lodash the right way
    So global lodash is not exported as global variable

--- src/modules/gherkin-annotator/index.ts
+++ src/modules/gherkin-annotator/index.ts
@@ -1,4 +1,4 @@
-import { debounce } from 'lodash'
+import debounce from 'lodash/debounce'
 import GherkinLinter, { OnParseCallback } from '../../lib/gherkin-linter'
 import { LanguageIdentifier } from '../../lib/gherkin-languages'
 
