c59edbd43a0791a83ad9fa16b22149f663fe0315
Mathias COQBLIN <mathias.coqblin@hiptest.net>
2022-04-01 00:00:03 +0200

    Upgrade jest to 27

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  testEnvironment: 'jsdom',
   transformIgnorePatterns: ['node_modules/(?!escape-string-regexp/)']
 }
