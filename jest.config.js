25f7973ee7d485fbd5f9a099358c7a7fb8ed0953
Mathias COQBLIN <mathias.coqblin@hiptest.net>
2022-05-04 10:03:02 +0200

    Upgrade jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['node_modules/(?!escape-string-regexp/)']
+  transformIgnorePatterns: ['node_modules/(?!escape-string-regexp|uuid/)']
 }
